36959bfee58e838adb49896e3a6294a054a59a1b
Melker Ulander <melker.ulander@pm.me>
2024-04-15 17:13:22 +0200

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mawkler/go-web-server
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+	port := "8080"
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: corsMux,
+	}
+
+	fmt.Printf("Listening to port %s", port)
+	server.ListenAndServe()
+}
